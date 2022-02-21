6e140e893a113235f1de655343d36e6494c840a5
Viney <vineykhaneja999@gmail.com>
2022-02-21 14:13:08 +0530

    [Viney] FEATURE ADDED: Calculating Daily EmpWage

--- Program.cs
+++ Program.cs
@@ -13,20 +13,27 @@ namespace EmpWage_OOPS
             //CONSTATNTS
             const int ISPRESENT = 1;
             const int ISABSENT = 0;
+            const int WAGEPERHOUR = 20;
+            const int FULLDAYHOUR = 8;
             //VARIABLES
             int empCheck;
+            int workingHour;
 
             Random random = new Random();
             empCheck = random.Next(0, 2);
+            //Checking Absent & present & assigning workinghours.
             switch (empCheck)
             {
                 case ISPRESENT:
                     Console.WriteLine("Employee is present");
+                    workingHour = FULLDAYHOUR;
                     break;
                 default:
                     Console.WriteLine("Employee is absent");
+                    workingHour = 0;
                     break;
             }
+            Console.WriteLine($"Daily EmpWage is {workingHour * WAGEPERHOUR}");
 
         }
     }
