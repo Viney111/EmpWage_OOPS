ce507d2500014537a798179003edeff1af597645
Viney <vineykhaneja999@gmail.com>
2022-02-21 14:07:34 +0530

    [Viney] FEATURE ADDED: Checking Attendance

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace EmpWage_OOPS
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmpWage by OOPS Program");
+            //CONSTATNTS
+            const int ISPRESENT = 1;
+            const int ISABSENT = 0;
+            //VARIABLES
+            int empCheck;
+
+            Random random = new Random();
+            empCheck = random.Next(0, 2);
+            switch (empCheck)
+            {
+                case ISPRESENT:
+                    Console.WriteLine("Employee is present");
+                    break;
+                default:
+                    Console.WriteLine("Employee is absent");
+                    break;
+            }
+
         }
     }
 }
